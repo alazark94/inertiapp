bf458104fb6c3c130db3af8e77d77f6356a88636
Alazar Kassahun <alazar.kas@gmail.com>
2022-02-01 00:06:50 +0300

    done with basic parts

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,4 @@ mix.js("resources/js/app.js", "public/js")
     .extract()
     .vue(3)
     .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
-    .version()
-    .browserSync("inertiapp.test");
+    .version();
